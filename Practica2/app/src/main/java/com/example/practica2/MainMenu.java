55c4f836a55c815821b867ffd081f772b82b542e
Sebaaaaas <sebsan01@ucm.es>
2023-11-30 19:46:39 +0100

    Añadidos muchos png y json, y 2 botones menu inicio

--- Practica2/app/src/main/java/com/example/practica2/MainMenu.java
+++ Practica2/app/src/main/java/com/example/practica2/MainMenu.java
@@ -9,32 +9,39 @@ import java.util.ArrayList;
 public class MainMenu extends Scene {
 
     Button playButton;
+    Button partidaRapida;
+    Button explorarMundos;
     public MainMenu(Logic logic){
         super(logic);
         log=logic;
 
         addGameObject(new SceneText(150,100,0,0,"MASTERMIND"));
 
-        playButton = new Button(100,400,150,50,"JUGAR", 0xFF1FE3E0);
-
+        //playButton = new Button(100,400,150,50,"JUGAR", 0xFF1FE3E0);
+        partidaRapida = new Button(100,300,150,50,"PARTIDA RAPIDA", 0xFF1FE3E0);
+        explorarMundos = new Button(100,400,150,50,"EXPLORAR MUNDOS", 0xFF1FE3E0);
     }
 
     @Override
     public void render(AndrGraphics2D graph) {
         super.render(graph);
-        playButton.render(graph);
+        partidaRapida.render(graph);
+        explorarMundos.render(graph);
     }
 
     @Override
     public void update(double deltaTime) {
         super.update(deltaTime);
-        playButton.update(deltaTime);
+        partidaRapida.update(deltaTime);
+        explorarMundos.update(deltaTime);
     }
 
     @Override
     public void handleInput(ArrayList<TouchEvent> event) {
         super.handleInput(event);
-        if(playButton.handleInput(event))
+        if(partidaRapida.handleInput(event))
+            playButtonEffect();
+        if(explorarMundos.handleInput(event))
             playButtonEffect();
     }
 
