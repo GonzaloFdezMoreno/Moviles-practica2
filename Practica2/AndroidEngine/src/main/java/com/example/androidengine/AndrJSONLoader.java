3b95efb38e304f2f7faeff93a7a780dc874ad6d7
Sebaaaaas <sebsan01@ucm.es>
2023-12-10 17:26:00 +0100

    arreglo?

--- Practica2/AndroidEngine/src/main/java/com/example/androidengine/AndrJSONLoader.java
+++ Practica2/AndroidEngine/src/main/java/com/example/androidengine/AndrJSONLoader.java
@@ -2,22 +2,9 @@ package com.example.androidengine;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.util.JsonReader;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.Object;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AndrJSONLoader {
     private AssetManager aMngr;
