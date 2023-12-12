f1d665c10377cd588bbdfc7c928fffb9c308f122
Sebaaaaas <sebsan01@ucm.es>
2023-12-12 22:12:03 +0100

    Cargamos todos los archivos de nivel para jugar

--- Practica2/app/src/main/java/com/example/practica2/WorldLevelSelector.java
+++ Practica2/app/src/main/java/com/example/practica2/WorldLevelSelector.java
@@ -17,6 +17,7 @@ public class WorldLevelSelector {
     int numLevels = 0;
     int widthMargin = 120, heightMargin = 120; //margenes entre botones de niveles
     ArrayList<Button> worldLevels;
+    String[] worldLevelFileNames;
     Logic log;
     WorldLevelSelector(int posX_, int posY_, Logic log_){
         posX = posX_; posY = posY_;
@@ -38,11 +39,12 @@ public class WorldLevelSelector {
 
     void getLevels(){ //cogemos los archivos en la carpeta correspondiente al mundo y asignamos cuantos hay
         try{
-            String[] levelNames = log.getEngine().getaJsonlodr().getAssetsDirectory("levels/world1");
-            numLevels = levelNames.length;
+            worldLevelFileNames = log.getEngine().getaJsonlodr().getAssetsDirectory("levels/world1");
+            numLevels = worldLevelFileNames.length;
             int i = 1;
-            for(String s : levelNames){
-                System.out.print("Level: " + i + ": " + s + "\n");
+            for(String s : worldLevelFileNames){
+                System.out.print("Level " + i + ": " + s + "\n");
+
                 ++i;
             }
         }
@@ -58,7 +60,7 @@ public class WorldLevelSelector {
     }
 
     public void handleInput(ArrayList<TouchEvent> event){
-        int i = 1;
+        int i = 0;
         for(Button b : worldLevels){
             if(b.handleInput(event)){
                 try{
@@ -73,12 +75,7 @@ public class WorldLevelSelector {
     }
 
     void testLoadJSON(int n) throws IOException {
-        String filename = "levels/world1/level_1_";
-
-        if(n < 10)
-            filename += "0" + String.valueOf(n) + ".json";
-        else
-            filename += String.valueOf(n) + ".json";
+        String filename = "levels/world1/" + worldLevelFileNames[n];
 
         InputStream is = log.getEngine().getaJsonlodr().getResource(filename);
 
