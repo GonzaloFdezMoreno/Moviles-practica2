7658a04882dabf5426f175c6c1e2300ec755418b
Sebaaaaas <sebsan01@ucm.es>
2023-12-12 23:21:00 +0100

    imagenes fondo  mundos

--- Practica2/app/src/main/java/com/example/practica2/ExploreWorldsScene.java
+++ Practica2/app/src/main/java/com/example/practica2/ExploreWorldsScene.java
@@ -10,20 +10,29 @@ public class ExploreWorldsScene extends Scene{
     Button nextWorld;
     Button prevWorld;
     //ArrayList<WorldLevelSelector> levelSelector;
+    int currentWorld = 0;
     WorldLevelSelector levelSelector;
 
+    String[] worldBackgroundImages = {"world1_background.jpg", "world2_bg.jpeg", "world3_bg.jpg", "world4_bg.jpeg"};
     protected ExploreWorldsScene(Logic logic) {
         super(logic);
 
         levelSelector = new WorldLevelSelector(30,30, logic);
+
+
+        prevWorld = new Button(10,10,30,40, "prev", 0XFFD3D3D3, log.currEngine.getAudio());
+        nextWorld = new Button(350,10,30,40, "next", 0XFFD3D3D3, log.currEngine.getAudio());
+
         //levelSelector = new ArrayList<>();
     }
 
     @Override
     public void render(AndrGraphics2D graph) {
         super.render(graph);
-        graph.drawImage(graph.createImage("world1_background.jpg"),0,0,500,800);
+        graph.drawImage(graph.createImage("world4_bg.jpeg"),0,0,500,800);
 
+        prevWorld.render(graph);
+        nextWorld.render(graph);
         levelSelector.render(graph);
         /*for(WorldLevelSelector wls : levelSelector)
             wls.render(graph);*/
